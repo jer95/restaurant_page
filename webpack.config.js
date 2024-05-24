0328df666d2fe05095bda05c5e9199bd94fd2b31
Jeremiah Dulla <jeremiahdulla@gmail.com>
2024-05-24 17:39:53 -0400

    add img processor

--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,12 @@ module.exports = {
     rules:[{
       test:/\.css$/i,
       use: ['style-loader','css-loader'],
-    }],
+    },
+    {
+      test:/\.(png|svg|jpg|jpeg|gif)$/i,
+      type:'asset/resource',
+    },
+  ],
   },
   plugins: [
     new HtmlWebpackPlugin({
