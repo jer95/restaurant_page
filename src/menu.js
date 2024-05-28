4437a912aead95e55996428ac61db87c1c30ee00
Jeremiah Dulla <jeremiahdulla@gmail.com>
2024-05-27 23:03:20 -0400

    Add sides & beverages

--- src/menu.js
+++ src/menu.js
@@ -6,16 +6,24 @@ import thirdMain from './imgs/main3.jpeg'
 import fourthMain from './imgs/main4.jpeg'
 import fifthMain from './imgs/main5.jpeg'
 import sixthMain from './imgs/main6.jpeg'
+import firstSide from './imgs/side1.jpeg'
+import secondSide from './imgs/side2.jpeg'
+import thirdSide from './imgs/side3.jpeg'
+import fourthSide from './imgs/side4.jpeg'
+import fifthSide from './imgs/side5.jpeg'
+import firstBeverage from './imgs/bev1.png'
+import secondBeverage from './imgs/bev2.png'
+import thirdBeverage from './imgs/bev3.png'
 
 
 export function menu (){
-    
+
     const content = document.querySelector('#content')
 
     //append menu header
 
     const header1 = document.createElement('h1')
-    header1.textContent = 'Our Menu'
+    header1.textContent = 'MENU'
     content.appendChild(header1)
 
     //append bbq logo to header
@@ -25,10 +33,12 @@ export function menu (){
     logo.classList.add('logo')
     header1.appendChild(logo)
 
+    //MAINS
+
     //append 'mains' header
 
     const mainHeader = document.createElement('h2')
-    mainHeader.textContent = 'Mains'
+    mainHeader.textContent = 'MAINS'
     content.appendChild(mainHeader)
 
     //append main 1 img
@@ -138,11 +148,188 @@ export function menu (){
 
     //append main 6 description & price
 
-    const mainInfo6 = document.createElement('p')
+    const mainInfo6 = document.createElement('p');
     const mainPrice6 = document.createElement('span');
     mainInfo6.classList.add('info');
     mainInfo6.textContent = 'Wings & Third Rack Ribs with a side of your choice.';
     mainPrice6.textContent = '$18.99';
     content.appendChild(mainInfo6);
     mainInfo6.appendChild(mainPrice6);
+
+    
+    
+    
+    // SIDES
+
+
+
+    //append 'sides' header
+
+    const sidesHeader = document.createElement('h2');
+    sidesHeader.textContent = 'SIDES';
+    content.appendChild(sidesHeader);
+
+    //append side 1 img
+
+    const sideContainer1 = document.createElement('div');
+    const side1 = document.createElement('img');
+    sideContainer1.classList.add('menu-container');
+    side1.src = firstSide;
+    sideContainer1.appendChild(side1);
+    content.appendChild(sideContainer1);
+
+    //append side 1 description & price
+
+    const sideInfo1 = document.createElement('p');
+    const sidePrice1 = document.createElement('span');
+    sideInfo1.classList.add('info')
+    sideInfo1.textContent = 'Fresh Cut Fries';
+    sidePrice1.textContent = '$3.99';
+    content.appendChild(sideInfo1);
+    sideInfo1.appendChild(sidePrice1);
+
+    //append side 2 img
+
+    const sideContainer2 = document.createElement('div');
+    const side2 = document.createElement('img');
+    sideContainer2.classList.add('menu-container');
+    side2.src = secondSide;
+    sideContainer2.appendChild(side2);
+    content.appendChild(sideContainer2);
+
+    //append side 2 description & price
+
+    const sideInfo2 = document.createElement('p');
+    const sidePrice2 = document.createElement('span');
+    sideInfo2.classList.add('info');
+    sideInfo2.textContent = 'Sweet Potato Fries';
+    sidePrice2.textContent = '$5.99';
+    content.appendChild(sideInfo2);
+    sideInfo2.appendChild(sidePrice2)
+
+    //append side 3 img
+
+    const sideContainer3 = document.createElement('div');
+    const side3 = document.createElement('img');
+    sideContainer3.classList.add('menu-container');
+    side3.src = thirdSide;
+    sideContainer3.appendChild(side3)
+    content.appendChild(sideContainer3);
+
+    //append side 3 description & price
+
+    const sideInfo3 = document.createElement('p');
+    const sidePrice3 = document.createElement('span');
+    sideInfo3.classList.add('info')
+    sideInfo3.textContent = 'Creamy Coleslaw';
+    sidePrice3.textContent = '$3.49';
+    content.appendChild(sideInfo3);
+    sideInfo3.appendChild(sidePrice3)
+
+    //append side 4 img
+
+    const sideContainer4 = document.createElement('div');
+    const side4 = document.createElement('img');
+    sideContainer4.classList.add('menu-container');
+    side4.src = fourthSide;
+    sideContainer4.appendChild(side4);
+    content.appendChild(sideContainer4);
+
+    //append side 4 description & price
+
+    const sideInfo4 = document.createElement('p');
+    const sidePrice4 = document.createElement('span');
+    sideInfo4.classList.add('info');
+    sideInfo4.textContent = 'Caesar Salad';
+    sidePrice4.textContent = '$4.99';
+    content.appendChild(sideInfo4);
+    sideInfo4.appendChild(sidePrice4);
+
+    //append side 5 img
+
+    const sideContainer5 = document.createElement('div');
+    const side5 = document.createElement('img');
+    sideContainer5.classList.add('menu-container');
+    side5.src = fifthSide;
+    sideContainer5.appendChild(side5);
+    content.appendChild(sideContainer5);
+
+    //append side 5 description & price
+
+    const sideInfo5 = document.createElement('p');
+    const sidePrice5 = document.createElement('span');
+    sideInfo5.classList.add('info');
+    sideInfo5.textContent = 'Loaded Baked Potato';
+    sidePrice5.textContent = '$3.99'
+    content.appendChild(sideInfo5);
+    sideInfo5.appendChild(sidePrice5);
+
+   
+   
+    // BEVERAGES
+
+    //append 'Beverages' header
+
+    const bevHeader = document.createElement('h2');
+    bevHeader.textContent = 'BEVERAGES'
+    content.appendChild(bevHeader)
+
+
+
+    //append bev 1 img
+
+    const bevContainer1 = document.createElement('div');
+    const bev1 = document.createElement('img');
+    bevContainer1.classList.add('menu-container');
+    bev1.src = firstBeverage;
+    bevContainer1.appendChild(bev1);
+    content.appendChild(bevContainer1);
+
+    //append bev 1 description & price
+
+    const bevInfo1 = document.createElement('p');
+    const bevPrice1 = document.createElement('span');
+    bevInfo1.classList.add('info');
+    bevInfo1.textContent = 'Canned Pop';
+    bevPrice1.textContent = '$2.99';
+    content.appendChild(bevInfo1);
+    bevInfo1.appendChild(bevPrice1);
+
+    //append bev 2 img
+
+    const bevContainer2 = document.createElement('div');
+    const bev2 = document.createElement('img');
+    bevContainer2.classList.add('menu-container');
+    bev2.src = secondBeverage;
+    bevContainer2.appendChild(bev2);
+    content.appendChild(bevContainer2);
+
+    //append bev 2 description & price
+
+    const bevInfo2 = document.createElement('p');
+    const bevPrice2 = document.createElement('span');
+    bevInfo2.classList.add('info');
+    bevInfo2.textContent = 'Canned Beer'
+    bevPrice2.textContent = '$3.99';
+    content.appendChild(bevInfo2);
+    bevInfo2.appendChild(bevPrice2);
+
+    //append bev 3 img
+
+    const bevContainer3 = document.createElement('div');
+    const bev3 = document.createElement('img');
+    bevContainer3.classList.add('menu-container');
+    bev3.src = thirdBeverage;
+    bevContainer3.appendChild(bev3);
+    content.appendChild(bevContainer3);
+
+    //append bev 3 description & price 
+
+    const bevInfo3 = document.createElement('p');
+    const bevPrice3 = document.createElement('span');
+    bevInfo3.classList.add('info');
+    bevInfo3.textContent = 'Bottled Beer'
+    bevPrice3.textContent = '$5.99'
+    content.appendChild(bevInfo3);
+    bevInfo3.appendChild(bevPrice3);
 }
